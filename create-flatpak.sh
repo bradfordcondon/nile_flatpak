331b71a9c78db880f65a9d47bc078224ca72bbc0
Bradford Condon <bradford.condon@gmail.com>
2024-09-06 22:33:49 -0400

    closer

--- create-flatpak.sh
+++ create-flatpak.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//45' pycryptodome
-~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//45' pyjson5
-~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//45' zstandard
-~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//45' requests
-~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//45' charset-normalizer
 
-flatpak-builder nile  flatpak.yaml --force-clean  
-flatpak-builder --user --install --force-clean nile flatpak.yaml
-flatpak build-bundle ~/.local/share/flatpak/repo nile.flatpak com.github.imLinguin.nile
+
+#~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//45' pycryptodome
+#~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//45' pyjson5
+#~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//45' zstandard
+#~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//45' requests
+#~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//45' charset-normalizer
+
+flatpak-builder nile  com.github.imLinguin.nile.yaml --force-clean  
+flatpak-builder --user --install --force-clean nile com.github.imLinguin.nile.yaml
+flatpak build-bundle ~/.local/share/flatpak/repo nile.flatpak com.github.imLinguin.nile.yaml
