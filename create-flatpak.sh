f77f0519f30229b9686ba81343a2c96a67916028
Bradford Condon <bradford.condon@gmail.com>
2024-09-06 22:48:48 -0400

    successfull flatpak out

--- create-flatpak.sh
+++ create-flatpak.sh
@@ -10,4 +10,4 @@
 
 flatpak-builder nile  com.github.imLinguin.nile.yaml --force-clean  
 flatpak-builder --user --install --force-clean nile com.github.imLinguin.nile.yaml
-flatpak build-bundle ~/.local/share/flatpak/repo nile.flatpak com.github.imLinguin.nile.yaml
+flatpak build-bundle ~/.local/share/flatpak/repo nile.flatpak com.github.imLinguin.nile
