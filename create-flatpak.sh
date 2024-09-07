68ba94d0036c15729a6be637ff5781896fd23752
Bradford Condon <bradford.condon@gmail.com>
2024-09-06 20:25:53 -0400

    add pycrpto etc

---
+++ create-flatpak.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//45' pycryptodome
+~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//45' pyjson5
+~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//45' pyzstandard
+~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//45' pyrequests
+~/work/flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//45' charset-normalizer
+
+flatpak-builder nile  flatpak.yaml --force-clean  
+flatpak-builder --user --install --force-clean nile flatpak.yaml
+flatpak build-bundle ~/.local/share/flatpak/repo nile.flatpak com.github.imLinguin.nile
